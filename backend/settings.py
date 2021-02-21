74a8743ca2ffe4562f6ba8ad4f6859f44c0f38e3
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2021-02-20 20:22:47 -0800

    possible fix

--- backend/settings.py
+++ backend/settings.py
@@ -215,6 +215,4 @@ AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'alecreations'
 
 if os.getcwd() == '/app':
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWRDED_PROTO', 'https')
-    SECURE_SSL_REDIRECT = True
     DEBUG = False
